1d90ad6aa7686fa82c35790f4ea5bcf1bd12ba8a
navneet.namankaur1313@gmail.com <115049676+Nav6k@users.noreply.github.com>
2023-10-22 15:03:03 +0530

    Update TicTacToe.jsx

--- src/components/TicTacToe.jsx
+++ src/components/TicTacToe.jsx
@@ -1,31 +1,84 @@
 import "./TicTacToe.css";
-import React from "react";
-function TicTacToe() {
-  return (
-    <div>
-      <div className="players">
-        <div className="player1"></div>
-        <div className="player2"></div>
-      </div>
+import React, { useState } from "react";
 
-      <div className="container">
-        <div className="players"></div>
-        <div className="main_box">
-          <div className="nine_box_container">
-            <div class="flex-container">
-              <div class="flex-item"></div>
-              <div class="flex-item"></div>
-              <div class="flex-item"></div>
-              <div class="flex-item"></div>
-              <div class="flex-item"></div>
-              <div class="flex-item"></div>
-              <div class="flex-item"></div>
-              <div class="flex-item"></div>
-              <div class="flex-item"></div>
+function TicTacToe(props) {
+  const [state, setState] = useState(Array(9).fill(null));
+  const [isXTurn, setIsXTurn] = useState(true);
+
+  const checkWinner = () => {
+    const winnerLogic = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let logic of winnerLogic) {
+      const [a, b, c] = logic;
+      if (state[a] != null && state[a] === state[b] && state[a] === state[c]) {
+        return state[a];
+      }
+    }
+    return false;
+  };
+  const isWinner = checkWinner();
+
+  const handleClick = (index, event) => {
+    const copyState = [...state];
+    copyState[index] = isXTurn ? "X" : "O";
+    setState(copyState);
+    setIsXTurn(!isXTurn);
+
+    if (props.onClick) {
+      props.onClick(event);
+    }
+  };
+
+  const handleReset = () => {
+    setState(Array(9).fill(null));
+  };
+
+  return (
+    <div className="anotherpage">
+      {isWinner ? (
+        <>
+          {isWinner} Won the game
+          <button onClick={handleReset}>play again</button>
+        </>
+      ) : (
+        <>
+          <div className="players">
+            <div className="player1"></div>
+            <div className="player2"></div>
+          </div>
+          <div className="container">
+            <div className="players"></div>
+            <div className="main_box">
+              <div
+                className="nine_box_container"
+                onClick={(event) => props.onClick(event)}
+              >
+                <div class="flex-container">
+                  {state.map((value, index) => (
+                    <div
+                      key={index}
+                      class="flex-item"
+                      value={value}
+                      onClick={(event) => handleClick(index, event)}
+                    >
+                      {value}
+                    </div>
+                  ))}
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
+        </>
+      )}
     </div>
   );
 }
